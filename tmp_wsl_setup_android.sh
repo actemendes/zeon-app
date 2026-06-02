10cd2f57b34ba093c8299e7607bc231856bfcf47
actemendesё Имя <kuv2606@gmail.com>
2026-06-02 20:22:42 +0300

    Игнор "рабочих" и временных файлов

--- tmp_wsl_setup_android.sh
+++
@@ -1,7 +0,0 @@
-set -euo pipefail
-export ANDROID_HOME="$HOME/Android/Sdk"
-export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
-export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools"
-sdkmanager --version
-yes | sdkmanager --licenses >/tmp/sdk_licenses.log || true
-sdkmanager "platform-tools" "platforms;android-34" "build-tools;34.0.0" "ndk;28.2.13676358"
