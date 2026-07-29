54a28b4ae9215b7d247278b968284590d92f1695
actemendes zeon-pc <kuv2606@gmail.com>
2026-07-29 21:57:07 +0300

    fix(core): close started service observers on stop

---
+++ hiddify-core/v2/hcore/stop_test.go
@@ -0,0 +1,44 @@
+package hcore
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+type recordingStartedService struct {
+	closeServiceError error
+	calls             []string
+}
+
+func (s *recordingStartedService) CloseService() error {
+	s.calls = append(s.calls, "close_service")
+	return s.closeServiceError
+}
+
+func (s *recordingStartedService) Close() {
+	s.calls = append(s.calls, "close_observers")
+}
+
+func TestCloseStartedServiceAlwaysClosesObservers(t *testing.T) {
+	closeError := errors.New("close service")
+	for _, test := range []struct {
+		name string
+		err  error
+	}{
+		{name: "success"},
+		{name: "service error", err: closeError},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			service := &recordingStartedService{closeServiceError: test.err}
+			gotErr := closeStartedService(service)
+			if !errors.Is(gotErr, test.err) {
+				t.Fatalf("error = %v, want %v", gotErr, test.err)
+			}
+			wantCalls := []string{"close_service", "close_observers"}
+			if !reflect.DeepEqual(service.calls, wantCalls) {
+				t.Fatalf("calls = %v, want %v", service.calls, wantCalls)
+			}
+		})
+	}
+}
